a2a47a29ab82755037468bf8531a852972f93251
Hubert Lewandowski <lewandowski.hubert486@gmail.com>
2018-11-11 11:23:13 +0100

    Add project files.

---
+++ Little Synthesia/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace Little_Synthesia
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.notesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.instructionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.playMySongToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(12, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(575, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Write your notes in the field below following the instruction!";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.Location = new System.Drawing.Point(106, 94);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(481, 39);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(12, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(84, 25);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "TYPED:";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.notesToolStripMenuItem,
+            this.instructionToolStripMenuItem,
+            this.playMySongToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(610, 33);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // notesToolStripMenuItem
+            // 
+            this.notesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.saveToolStripMenuItem});
+            this.notesToolStripMenuItem.Name = "notesToolStripMenuItem";
+            this.notesToolStripMenuItem.Size = new System.Drawing.Size(83, 29);
+            this.notesToolStripMenuItem.Text = "Notes...";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(140, 30);
+            this.openToolStripMenuItem.Text = "Open";
+            // 
+            // saveToolStripMenuItem
+            // 
+            this.saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            this.saveToolStripMenuItem.Size = new System.Drawing.Size(140, 30);
+            this.saveToolStripMenuItem.Text = "Save";
+            // 
+            // instructionToolStripMenuItem
+            // 
+            this.instructionToolStripMenuItem.Name = "instructionToolStripMenuItem";
+            this.instructionToolStripMenuItem.Size = new System.Drawing.Size(141, 29);
+            this.instructionToolStripMenuItem.Text = "The Instruction";
+            this.instructionToolStripMenuItem.Click += new System.EventHandler(this.instructionToolStripMenuItem_Click);
+            // 
+            // playMySongToolStripMenuItem
+            // 
+            this.playMySongToolStripMenuItem.Name = "playMySongToolStripMenuItem";
+            this.playMySongToolStripMenuItem.Size = new System.Drawing.Size(154, 29);
+            this.playMySongToolStripMenuItem.Text = "PLAY MY SONG!";
+            this.playMySongToolStripMenuItem.Click += new System.EventHandler(this.playMySongToolStripMenuItem_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(12, 148);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(88, 25);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "NOTES:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox2.Location = new System.Drawing.Point(106, 139);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(481, 39);
+            this.textBox2.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(610, 201);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem notesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem instructionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem playMySongToolStripMenuItem;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
---
+++ Little Synthesia/Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.IO;
+using System.Media;
+using System.Windows.Forms;
+
+namespace Little_Synthesia
+{
+    public partial class Form1 : Form
+    {
+        static string rootPath = Path.GetFullPath(Path.Combine(Directory.GetCurrentDirectory(), "..\\..\\Notes\\\\"));
+
+        SoundPlayer Z = new SoundPlayer(rootPath + "c3.wav");
+        SoundPlayer S = new SoundPlayer(rootPath + "c-3.wav");
+        SoundPlayer X = new SoundPlayer(rootPath + "d3.wav");
+        SoundPlayer D = new SoundPlayer(rootPath + "d-3.wav");
+        SoundPlayer C = new SoundPlayer(rootPath + "e3.wav");
+        SoundPlayer V = new SoundPlayer(rootPath + "f3.wav");
+        SoundPlayer G = new SoundPlayer(rootPath + "f-3.wav");
+        SoundPlayer B = new SoundPlayer(rootPath + "g3.wav");
+        SoundPlayer H = new SoundPlayer(rootPath + "g-3.wav");
+        SoundPlayer N = new SoundPlayer(rootPath + "a3.wav");
+        SoundPlayer J = new SoundPlayer(rootPath + "a-3.wav");
+        SoundPlayer M = new SoundPlayer(rootPath + "b3.wav");
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void playSoundOnKey(string key)
+        {
+            switch (key.ToUpper())
+            {
+                case "Z": Z.Play(); break;
+                case "S": S.Play(); break;
+                case "X": X.Play(); break;
+                case "D": D.Play(); break;
+                case "C": C.Play(); break;
+                case "V": V.Play(); break;
+                case "G": G.Play(); break;
+                case "B": B.Play(); break;
+                case "H": H.Play(); break;
+                case "N": N.Play(); break;
+                case "J": J.Play(); break;
+                case "M": M.Play(); break;
+                default: break;
+            }
+        }
+
+        private void instructionToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("To start typing the music notes you have to use your computer keyboard.\nSo make sure you've got one there (Especially kashubian keyboard).", "The Instruction 1/3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("Real piano keys are associated with these on your keyboard. The structure of them is also really similar.\n\nWhite keys are from Z to M.\nBlack keys are: S, D, G, H, J.", "The Instruction 2/3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            MessageBox.Show("It looks quite complicated for now, but soon or later you'll find it's dope. Don't get frustrated too easily and remember to have fun ;)", "The Instruction 3/3", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = ""; // Clear textbox2;
+
+            if (textBox1.Text == "") return;
+            playSoundOnKey(textBox1.Text[textBox1.TextLength - 1].ToString().ToUpper());
+
+            for (int i = 0; i < textBox1.Text.Length; i++)
+            {
+                switch (textBox1.Text[i].ToString().ToUpper())
+                {
+                    case "Z": textBox2.Text += "C "; break;
+                    case "S": textBox2.Text += "#C "; break;
+                    case "X": textBox2.Text += "D "; break;
+                    case "D": textBox2.Text += "#D "; break;
+                    case "C": textBox2.Text += "E "; break;
+                    case "V": textBox2.Text += "F "; break;
+                    case "G": textBox2.Text += "#F "; break;
+                    case "B": textBox2.Text += "G "; break;
+                    case "H": textBox2.Text += "#G "; break;
+                    case "N": textBox2.Text += "A "; break;
+                    case "J": textBox2.Text += "#A "; break;
+                    case "M": textBox2.Text += "H "; break;
+                    default: break;
+                }
+            }
+        }
+
+        private void playMySongToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Z.Play();
+        }
+    }
+}
